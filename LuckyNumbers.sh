af2599f336211c33bf99e8daa2e908c6e9751062
14atg3@queensu.ca <14atg3@queensu.ca>
2016-11-14 17:56:42 -0500

    Second commit Q1

--- LuckyNumbers.sh
+++ LuckyNumbers.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Josh Burak, Alexander Gadanidis, Kat Pinto, Matthew Sadecki
+# , 10186175, , 10179995
 echo "Lucky numbers:"
 y=1000;
 while [ $y -lt 10000 ]
@@ -7,5 +9,10 @@ x=$y; digit=0; sum=0;
 while [ $x -gt 0 ]
 do
 digit=$(( $x % 10 )); x=$(( $x / 10 )); sum=$(( $sum + $digit ))
+done
+x=$sum; sumdigits=0
+while [ $x -gt 0 ]
+do
+digit=$(( $x % 10 )); x=$(( $x / 10 )); sumdigits=$(( $sumdigits + $digit ))
 done
 done
