5cdf897f412647cc72a992310179c7fc029d2093
Flanzenberg <14jrb3@queensu.ca>
2016-11-14 18:46:15 -0500

    Update FINALquestion5.sh

--- FINALquestion5.sh
+++ FINALquestion5.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#Josh Burak, Alexander Gadanidis, Kat Pinto, Matthew Sadecki
+#10169785 , 10186175, 10168919 , 10179995
 for file in /dir/*
 do
 	sed -i 'ls/^/#Developed in cisc220\n/' $file
